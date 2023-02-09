ca0ee317fb7692cf0ee8f2abe487203ca697879b
Ahmet Rusen Karaoglu <87625650+ARKaraoglu@users.noreply.github.com>
2023-02-09 11:16:28 -0500

    finished desktop, started working mobile

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const allFaqElements = document.querySelectorAll(".faq-question-container");
 
 const allPElements = document.querySelectorAll(".p");
 
+const allArrows = document.querySelectorAll(".faq-arrow");
+
 for (let x = 0; x < allFaqElements.length; x++) {
     allFaqElements[x].addEventListener("click", function () {
         console.log(allFaqElements[x]);
         console.log(allPElements[x].textContent);
 
+        allArrows[x].classList.toggle("arrow-twist");
         allPElements[x].classList.toggle("display-none");
     });
 }
